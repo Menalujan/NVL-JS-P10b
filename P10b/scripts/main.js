ba9669e31960ceb00cca7122b44a2e1e854a4602
Menalujan <56971849+Menalujan@users.noreply.github.com>
2019-12-06 20:21:05 +0100

    Add files via upload

---
+++ P10b/scripts/main.js
@@ -0,0 +1,15 @@
+var numeros = [];
+
+    for(i = 0; i < 2; i++){
+        Math.floor((Math.random() * 100) + 1).push(numeros);
+    }
+var pares = [];
+var impares = [];
+
+    if(numero %2 == 0){
+    push(pares);
+    }else{
+    push(impares);
+    }
+    alert(pares);
+    alert(impares);
